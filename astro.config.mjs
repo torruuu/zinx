537b4bc1e357214dc6f0277c52f4388ef02c40dc
Alejandro <ale.torrubiano@gmail.com>
2024-04-18 15:50:32 +0200

    feat: added global sass config and alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@assets/styles/_mixins.scss";
+            @import "@assets/styles/_variables.scss";
+            @import "@assets/styles/main.scss";
+          `
+        }
+      }
+    }
+  }
+});
