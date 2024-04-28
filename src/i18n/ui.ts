727d6639773543227efc08125dd6011949889ee5
Alejandro <ale.torrubiano@gmail.com>
2024-04-28 13:30:28 +0200

    feat: multilanguages config with i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+  es: 'Español',
+  en: 'English',
+}
+
+export const defaultLang = 'es'
+
+export const ui = {
+  es: {
+    'nav.home': 'Inicio',
+    'nav.movies': 'Películas',
+    'nav.series': 'Series',
+    'nav.explore': 'Explora',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.movies': 'Movies',
+    'nav.explore': 'Explore',
+  },
+} as const
