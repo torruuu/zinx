5057e48b31319f763b7587288fa5371cc08575ed
Alejandro <ale.torrubiano@gmail.com>
2024-05-26 20:48:58 +0200

    feat: nav scroll background

---
+++ src/components/nav/NavBg.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+
+const scrolled = ref<boolean>(false)
+const handleScroll = () => (scrolled.value = window.scrollY > 20)
+
+onMounted(() => window.addEventListener('scroll', handleScroll))
+onUnmounted(() => window.removeEventListener('scroll', handleScroll))
+</script>
+
+<template>
+  <div class="nav-background" :class="{ scrolled: scrolled }"></div>
+</template>
+
+<style lang="scss" scoped>
+.nav-background {
+  background-color: transparent;
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  max-width: map-get($map: $sizes, $key: s-body);
+  width: 100%;
+  height: map-get($map: $sizes, $key: s-nav);
+  transition: all 300ms ease-in;
+}
+
+.scrolled {
+  background-color: map-get($map: $colors, $key: c-black-sec);
+}
+</style>
