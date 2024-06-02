f45fb56bc82efe2332c2c20826e0294994db7105
Alejandro <ale.torrubiano@gmail.com>
2024-06-02 13:36:10 +0200

    fix: scroll with stores

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -2,6 +2,7 @@
 import MainBanner from '@/components/home/MainBanner.vue'
 import RegularSlider from '@/components/common/RegularSlider.vue'
 import { $mainSectionMounted, $loadError } from '@/stores/data'
+import { $scrollData } from '@/stores/scroll'
 import { onMounted, onUnmounted, onBeforeUnmount, ref } from 'vue'
 import type { RegularSliderMedia, ui } from '@/types/index'
 
@@ -20,7 +21,7 @@ let timeoutId: number
 const scrollY = ref()
 const handleScroll = () => (scrollY.value = window.scrollY)
 const saveScrollPosition = () =>
-  sessionStorage.setItem(`${props.sectionTitle}Scroll`, scrollY.value)
+  $scrollData.setKey(`${props.sectionTitle}Scroll`, scrollY.value)
 
 onMounted(() => {
   mainSectionData.value = props.mainSection
@@ -30,9 +31,9 @@ onMounted(() => {
     if (!$mainSectionMounted.get()) $loadError.set(true)
   }, MAX_WAIT_TIME)
 
-  const sectionScroll = sessionStorage.getItem(`${props.sectionTitle}Scroll`)
-  if (sectionScroll) setTimeout(() => window.scrollTo(0, parseInt(sectionScroll)), 0)
-  sessionStorage.clear()
+  const sectionScroll = $scrollData.get()[`${props.sectionTitle}Scroll`]
+  if (sectionScroll) setTimeout(() => window.scrollTo(0, sectionScroll), 0)
+  $scrollData.set({})
   window.addEventListener('scroll', handleScroll)
   window.addEventListener('beforeunload', saveScrollPosition)
 })
---
+++ src/stores/scroll.ts
@@ -0,0 +1,3 @@
+import { map } from 'nanostores'
+
+export const $scrollData = map<{ [key: string]: number }>()
